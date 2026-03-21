a9164e263ea3b348958b8478bc28a124cc666e59
Pierre Marais <pierremarais67@gmail.com>
2026-03-21 11:15:34 +0100

    feat(web): move Spaces to second position in sidebar nav

--- web/src/lib/components/shared-components/side-bar/user-sidebar.svelte
+++ web/src/lib/components/shared-components/side-bar/user-sidebar.svelte
@@ -42,6 +42,8 @@
 <Sidebar ariaLabel={$t('primary')}>
   <NavbarItem title={$t('photos')} href={Route.photos()} icon={mdiImageMultipleOutline} activeIcon={mdiImageMultiple} />
 
+  <NavbarItem title={$t('spaces')} href={Route.spaces()} icon={mdiAccountGroupOutline} activeIcon={mdiAccountGroup} />
+
   {#if featureFlagsManager.value.search}
     <NavbarItem title={$t('explore')} href={Route.explore()} icon={mdiMagnify} />
   {/if}
@@ -64,8 +66,6 @@
     icon={mdiAccountMultipleOutline}
     activeIcon={mdiAccountMultiple}
   />
-
-  <NavbarItem title={$t('spaces')} href={Route.spaces()} icon={mdiAccountGroupOutline} activeIcon={mdiAccountGroup} />
 
   <NavbarGroup title={$t('library')} size="tiny" />
 
